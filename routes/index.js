eda95f2d201970de348d17966dc4f7ce2edc86af
Fernan de Dios <info@mediagramstudios.com>
2019-02-07 09:40:31 +0800

    added contact static content

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,14 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Node Video Collaboration' });
 });
 
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'Contact Us' });
+  })
+  .post(function(req, res, next) {
+    res.render('thanks', { title: 'Contact Us' });
+  });
+
+
+
 module.exports = router;
