de87b5426c0cc3cdf67fa8ada74660171c59e086
Fernan de Dios <info@mediagramstudios.com>
2019-02-07 09:18:56 +0800

    added static about page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Node Video Collaboration' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Node Video Collaboration' });
 });
 
 module.exports = router;
