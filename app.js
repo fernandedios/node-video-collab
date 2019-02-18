8f0832aaeece728388d8c0e6452924e6c5b3681a
Fernan de Dios <info@mediagramstudios.com>
2019-02-18 10:15:04 +0800

    added task view and integrate routs to app

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
+var taskRouter = require('./routes/task');
 
 require('./passport');
 
 mongoose.connect(config.dbConnstring);
 global.User = require('./models/user');
+global.Task = require('./models/task');
 
 var app = express();
 
@@ -46,6 +48,7 @@ app.use(function(req, res, next) {
 });
 app.use('/', indexRouter);
 app.use('/', authRouter);
+app.use('/', taskRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
