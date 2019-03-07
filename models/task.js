0b97fb2e940a44b7a220be904e26ca7f12ec9a38
Fernan de Dios <info@mediagramstudios.com>
2019-03-07 11:55:34 +0800

    updates to task model

--- models/task.js
+++ models/task.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var taskSchema = mongoose.Schema({
+var taskSchema = Schema({
   title: String,
   content: String,
-  chat: String
+  chat: String,
+  owner: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
