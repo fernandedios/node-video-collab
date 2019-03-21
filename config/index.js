ca3faef207fb11445e0c787065a45f603d2c7d56
Fernan de Dios <info@mediagramstudios.com>
2019-03-21 11:17:16 +0800

    update config index test value

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 else if (process.env.NODE_ENV === 'test') {
-	module.exports = require('./testing'); // export test keys
+	module.exports = require('./test-keys'); // export test keys
 }
 
 else {
