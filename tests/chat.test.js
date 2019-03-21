37d734d63fd01f318f5b9359d4d2191d8406c2fe
Fernan de Dios <info@mediagramstudios.com>
2019-03-21 13:33:34 +0800

    added login test

--- tests/chat.test.js
+++ tests/chat.test.js
@@ -1,3 +1,4 @@
+const expect = require('expect');
 const { ObjectID } = require('mongodb');
 const request = require('supertest');
 const app = require('../app');
@@ -6,10 +7,11 @@ const { tasks, populateTasks, users, populateUsers} = require('./seed');
 beforeEach(populateUsers);
 
 describe('POST /login', () => {
-  it('should login the user successfully', (done) => {
+  it('should login the user with right credentials successfully', (done) => {
     request(app)
-      .get('/')
-      .expect(200)
+      .post('/login')
+      .send({ email: users[0].email, password: users[0].password })
+      .expect(302)
       .end(done);
   });
 });
